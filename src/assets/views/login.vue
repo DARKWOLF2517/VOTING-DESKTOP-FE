4662434f4c2e934670a5a971983c02024b7d5aed
ueng-lin <queenrbahay@gmail.com>
2024-03-05 09:52:01 +0800

    login

--- src/assets/views/login.vue
+++ src/assets/views/login.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="h-screen w-screen "  style="background-image: url('/src/assets/images/login-bg.png'); background-size: cover; background-position: center;  margin: 0;
-  padding: 0;">
+  <div class="h-screen w-screen " >
     <!-- Navigation Bar -->
     <nav class="bg-primary p-2">
       <div class="container">
@@ -13,26 +12,33 @@
     </nav>
       <!-- Main Content -->
     <div class="container px-4 sm:px-8 lg:grid lg:grid-cols-2 lg:gap-x-8">
-      <div class="mb-16 lg:mt-32 xl:mt-20 xl:mr-12 px-10">
+      <div class="mb-16 lg:mt-32 xl:mt-20 xl:mr-12 px-10 ">
+        <div class="bg-white rounded p-4 h-full shadow-2xl">
           <!-- Logo -->
-        <div class="logo  flex justify-center">
+        <div class="logo  flex justify-center p-4">
           <img src="https://www.climbs.coop/wp-content/uploads/2021/10/climbs-logo.jpg" alt="Sample Image"
             class="w-20 h-20">
         </div>
       
         <h1 class="h1-large mb-2 text-primary font-bold">Register</h1>
-        <p class="p-large mb-8 ">Start getting things done by enetering registration code</p>
-        
+        <p class="p-large mb-8 ">Start by entering registration code</p>
+        <label for="registrationCode" class="flex text-start mb-0 font-bold text-primary">Registration code</label>
         <input v-model="inputText" type="text"
-          class="w-full mt-4 h-10 px-4 rounded-lg border focus:outline-none focus:border-blue-500 bg-white"
+          class="w-full h-10 px-4 rounded-lg border focus:outline-none border-blue-200 focus:border-blue-500 bg-gray-100"
           placeholder="Enter code...">
         <div class="mt-2 flex justify-end">
-          <button type="button" class="text-white bg-primary hover:bg-accent w-full md:w-auto md:mr-0">Proceed</button>
+          <button type="button" class="text-white bg-primary hover:bg-accent w-80 md:w-auto md:mr-0 flex justify-center items-center">
+  Proceed 
+  <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 8 14">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1"/>
+  </svg>
+</button>
         </div>
         <div class="mt-4 text-start">
             <small><b>Note:</b> All Cooperatives have their respective Registration Code, Use that to proceed here.</small>
           </div>
       </div>
+    </div>
       <div class="xl:text-right image-bg ">
         <img class="h-full bg-cover" src="/src/assets/images/welcome-bg.png" alt="alternative">
       </div>
