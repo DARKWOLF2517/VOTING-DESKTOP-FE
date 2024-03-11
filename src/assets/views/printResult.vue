1b9428b3f80c26666cf603cd4b627f83898ac967
DARKWOLF2517 <jerrichopamisa@gmail.com>
2024-03-11 16:44:04 +0800

    asdf

--- src/assets/views/printResult.vue
+++ src/assets/views/printResult.vue
@@ -3,14 +3,14 @@
         <div class="w-72 md:w-96 bg-white border border-gray-300 rounded-lg p-4">
             <div>
                 <div class="ballot-header ">
-                    <p class="flex justify-start"><b>BALLOT CODE: </b></p>
-                    <p class=" flex justify-start mb-2"><b>Date Time Printed:</b></p>
+                    <p class="flex justify-start"><b>BALLOT CODE:{{ }} </b></p>
+                    <p class=" flex justify-start mb-2"><b>Date Time Printed: {{ date }}</b></p>
                     <h3 class=" flex justify-start">List of Candidates Voted</h3>
                 </div>
                 <div class="overflow-y-auto " style="max-height: 80vh;">
-                    <ul class="bg-white">
+                    <ul class="bg-white" v-for="candidates in result">
                         <div class="p-2 bg-gray-600 text-white">
-                            <h3 class="text-lg font-semibold ">Name of Position</h3>
+                            <h3 class="text-lg font-semibold ">Name of Position {{  }}</h3>
                             <!-- <p class="text-center">Select 3 Candidates</p> -->
                         </div>
                         <li>
@@ -42,3 +42,45 @@
     </div>
 
 </template>
+
+<script lang="ts">
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            baseUrl: import.meta.env.VITE_APP_BASE_URL,
+            result: [],
+            date: '',
+        }
+    },
+    mounted() {
+        console.log('asdf')
+        const baseUrl = window.location.origin;
+        console.log(baseUrl);
+        this.fetchData();
+
+        //get date
+        const now = new Date();
+        const formattedDateTime = now.toLocaleString(); // You can use other formatting methods as well
+        this.date = formattedDateTime;
+    },
+    methods: {
+        async fetchData() {
+            try {
+                const getData = await this.axios.get(this.baseUrl + 'api/fetchVoteResult/' + 145942104);
+                console.log(getData.data);
+                this.result = getData.data;
+                // this.$router.push(getDataPrint.data);
+                // Your Laravel backend base URL
+
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+
+}
+
+
+
+</script>
